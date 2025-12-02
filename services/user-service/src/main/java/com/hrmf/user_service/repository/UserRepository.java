5728f5c64a4e3dc6f596f15f11b09405f0f3e450
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-02 22:03:27 +0600

    creating project

---
+++ services/user-service/src/main/java/com/hrmf/user_service/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.hrmf.user_service.repository;
+
+import com.hrmf.user_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.userRoles ur LEFT JOIN FETCH ur.role WHERE u.email = :email")
+    Optional<User> findByEmailWithRoles(@Param("email") String email);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.personalDetails WHERE u.id = :userId")
+    Optional<User> findByIdWithPersonalDetails(@Param("userId") UUID userId);
+}
