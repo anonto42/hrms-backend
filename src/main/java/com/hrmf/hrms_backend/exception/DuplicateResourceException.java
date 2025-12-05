8af55f94a9e248a6ffb9b2d526d50a1c0562e75a
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-05 14:09:42 +0600

    exceptions

---
+++ src/main/java/com/hrmf/hrms_backend/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.hrmf.hrms_backend.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/hrmf/hrms_backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hrmf.hrms_backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
