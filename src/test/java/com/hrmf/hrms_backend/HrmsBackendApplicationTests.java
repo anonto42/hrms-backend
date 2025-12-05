d4d60541246c875d534aaa91b194fec1ed4eb25f
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-05 14:13:55 +0600

    main app and test

---
+++ src/test/java/com/hrmf/hrms_backend/HrmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmf.hrms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
