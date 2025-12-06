538a82130bdfaea4b4262250fd5b090c4dcedfed
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-06 23:01:34 +0600

    Sign in request DTO updated

--- src/main/java/com/hrmf/hrms_backend/dto/auth/SignInRequestDto.java
+++ src/main/java/com/hrmf/hrms_backend/dto/auth/SignInRequestDto.java
@@ -3,9 +3,15 @@ package com.hrmf.hrms_backend.dto.auth;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignInRequestDto {
 
     @NotBlank(message = "You must give the email!")
@@ -13,6 +19,5 @@ public class SignInRequestDto {
     private String email;
 
     @NotBlank(message = "You must give the password!")
-    @Min(value = 6,message = "Password must be atlas 6 characters!")
     private String password;
 }
