cdf193e187c0667d558462de1509361424a71348
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-07 00:24:57 +0600

    Verify otp request dto

---
+++ src/main/java/com/hrmf/hrms_backend/dto/auth/VerifyOtpRequestDto.java
@@ -0,0 +1,24 @@
+package com.hrmf.hrms_backend.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyOtpRequestDto {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "OTP is required")
+    @Size(min = 6, max = 6, message = "OTP must be 6 digits")
+    private String otp;
+}
