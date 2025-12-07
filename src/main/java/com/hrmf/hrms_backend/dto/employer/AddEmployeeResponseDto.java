097c405813af0ebf5979665045b65c6a98683c6a
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-07 22:28:56 +0600

    added required annotations

--- src/main/java/com/hrmf/hrms_backend/dto/employer/AddEmployeeResponseDto.java
+++ src/main/java/com/hrmf/hrms_backend/dto/employer/AddEmployeeResponseDto.java
@@ -1,8 +1,14 @@
 package com.hrmf.hrms_backend.dto.employer;
 
 import com.hrmf.hrms_backend.enums.UserRole;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AddEmployeeResponseDto {
     private String id;
