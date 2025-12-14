38f597c757e4fd265d043a0db82159f10c23f2c2
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-14 15:16:22 +0600

    Leave enums

---
+++ src/main/java/com/hrmf/hrms_backend/enums/LeaveType.java
@@ -0,0 +1,11 @@
+package com.hrmf.hrms_backend.enums;
+
+public enum LeaveType {
+    SICK_LEAVE,
+    CASUAL_LEAVE,
+    ANNUAL_LEAVE,
+    MATERNITY_LEAVE,
+    PATERNITY_LEAVE,
+    UNPAID_LEAVE,
+    OTHER
+}
