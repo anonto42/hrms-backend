333214b707a9c53405cee75a05018f78883b6223
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-22 17:00:43 +0600

    Dot Env Config created

---
+++ src/main/java/com/hrmf/hrms_backend/config/DotenvConfig.java
@@ -0,0 +1,15 @@
+package com.hrmf.hrms_backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotenvConfig {
+
+    static {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach( e ->
+                System.setProperty(e.getKey(), e.getValue())
+        );
+    }
+}
