806079079f9f39e960586ab1c0b27a1c53643697
Sohidul Islam <anontom90@gmail.com>
2026-01-04 11:13:29 +0600

    main entry point

---
+++ src/main/java/com/hrmFirm/HrmFirmApplication.java
@@ -0,0 +1,13 @@
+package com.hrmFirm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmFirmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmFirmApplication.class, args);
+	}
+
+}
