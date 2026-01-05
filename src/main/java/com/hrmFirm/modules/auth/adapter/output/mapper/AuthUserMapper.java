fd12f128de1aab6d0ace8976af9274cff22e4263
Sohidul Islam <anontom90@gmail.com>
2026-01-06 00:49:19 +0600

    auth user mapper

--- src/main/java/com/hrmFirm/modules/auth/adapter/output/mapper/AuthUserMapper.java
+++ src/main/java/com/hrmFirm/modules/auth/adapter/output/mapper/AuthUserMapper.java
@@ -3,16 +3,27 @@ package com.hrmFirm.modules.auth.adapter.output.mapper;
 import com.hrmFirm.modules.auth.domain.AuthUser;
 import com.hrmFirm.modules.user.adapter.output.persistence.entity.UserEntity;
 
+import java.util.UUID;
+
 public class AuthUserMapper {
     public static AuthUser toAuthUser(UserEntity entity) {
+
+        UUID creatorId = null;
+        if (entity.getCreatedBy() != null)
+            creatorId = entity.getCreatedBy().getId();
+
+        UUID id = null;
+        if (entity.getId() != null)
+            id = entity.getId();
+
         return new AuthUser(
-                null,
+                id,
                 entity.getName(),
                 entity.getEmail(),
                 entity.getPassword(),
                 entity.getRole(),
                 entity.getStatus(),
-                entity.getCreatedBy().getId()
+                creatorId
         );
     }
 }
