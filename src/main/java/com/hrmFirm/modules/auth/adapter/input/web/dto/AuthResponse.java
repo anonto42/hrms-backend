dfefd56ea00914824caf014d2124a00491fb44f7
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-11 10:59:20 +0600

    deleted

--- src/main/java/com/hrmFirm/modules/auth/adapter/input/web/dto/AuthResponse.java
+++
@@ -1,9 +0,0 @@
-package com.hrmFirm.modules.auth.adapter.input.web.dto;
-
-import com.hrmFirm.modules.auth.domain.AuthToken;
-
-public record AuthResponse(String accessToken) {
-    public static AuthResponse from(AuthToken token) {
-        return new AuthResponse(token.accessToken());
-    }
-}
