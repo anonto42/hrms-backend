2f91fa6c76e840a0b76ecd37b86298583c4d39a5
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-11 10:58:12 +0600

    delete temp user

---
+++ src/main/java/com/hrmFirm/modules/tempUser/usecase/impl/DeleteTempUserImp.java
@@ -0,0 +1,19 @@
+package com.hrmFirm.modules.tempUser.usecase.impl;
+
+import com.hrmFirm.modules.tempUser.usecase.port.input.DeleteUserUseCase;
+import com.hrmFirm.modules.tempUser.usecase.port.output.TempUserRepositoryPort;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DeleteTempUserImp
+        implements DeleteUserUseCase {
+
+    private final TempUserRepositoryPort tempUserRepositoryPort;
+
+    @Override
+    public void delete(String email) {
+
+    }
+}
