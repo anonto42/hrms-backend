1ae43863d38cef3afb7894796993d0c672711ca2
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-11 10:52:23 +0600

    Rete Limit execution

---
+++ src/main/java/com/hrmFirm/common/exception/RateLimitExceededException.java
@@ -0,0 +1,9 @@
+package com.hrmFirm.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class RateLimitExceededException extends CustomException{
+    public RateLimitExceededException(String message) {
+        super(message, HttpStatus.TOO_MANY_REQUESTS);
+    }
+}
