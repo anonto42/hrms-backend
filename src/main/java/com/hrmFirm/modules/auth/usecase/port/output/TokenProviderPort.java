be9489ba40089c0511d1befa1e335c3a948bab83
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-14 01:29:18 +0600

    removed

--- src/main/java/com/hrmFirm/modules/auth/usecase/port/output/TokenProviderPort.java
+++
@@ -1,8 +0,0 @@
-package com.hrmFirm.modules.auth.usecase.port.output;
-
-import com.hrmFirm.modules.auth.domain.AuthToken;
-import com.hrmFirm.modules.auth.domain.AuthUser;
-
-public interface TokenProviderPort {
-    AuthToken generateToken(AuthUser user);
-}
