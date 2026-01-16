d37db3ee2f922f358041f76b290581dfe951452b
Sohidul Islam Ananto <anontom90@gmail.com>
2026-01-16 16:50:40 +0600

    added bootJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.9"
@@ -75,4 +77,8 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.named<BootJar>("bootJar") {
+	archiveFileName.set("app.jar")
 }
